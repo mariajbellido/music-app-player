d70a921aa1a4319e260ff482fc581ede39a4debd
Maria Jesus Bellido <mariajbellido@gmail.com>
2021-02-02 11:57:27 +0100

    initial commit - adding final project from repl.it

---
+++ script.js
@@ -0,0 +1,76 @@
+const songs = [
+
+"DREAMS.mp3",
+"FUNKYSUSPENSE.mp3",
+"HAPPYROCK.mp3",
+"HEY!.mp3",
+"MEMORIES.mp3",
+"RELAXING.mp3",
+"UKELELE.mp3",
+"GOINGHIGHER.mp3",
+
+]
+
+ const player = document.getElementById("player"); 
+
+function createSongList() {
+
+  const list = document.createElement("ol"); 
+  for (let i = 0; i < songs.length; i++) {
+    const item = document.createElement("li");
+    item.appendChild(document.createTextNode(songs[i]));
+    list.appendChild(item);
+  }
+  return list;
+}
+
+
+
+
+const songList = document.getElementById("songList")
+songList.appendChild(createSongList());
+
+const links = document.querySelectorAll("li"); 
+for (const link of links) {
+  link.addEventListener("click", setSong);
+}
+
+function setSong(e) {
+
+  document.querySelector("#headphones").classList.remove("pulse");
+
+  const source = document.getElementById("source");
+  source.src = "songs/" + e.target.innerText;
+
+  document.getElementById("currentSong").innerText = ` Now playing: ${e.target.innerText}`;
+
+ 
+  player.load();
+  player.play();
+
+document.querySelector("#headphones").classList.add("pulse");
+}
+
+
+function playAudio() {
+  if (player.readyState) {
+    player.play();
+  }
+}
+
+function pauseAudio() {
+  player.pause();
+}
+
+const slider = document.getElementById("volumeSlider"); 
+slider.oninput = function(e) {
+  const volume = e.target.value;
+  player.volume = volume; 
+}
+
+function updateProgress () {
+  if (player.currentTime > 0 ) {
+  const progressBar = document.getElementById("progress");
+  progressBar.value = (player.currentTime / player.duration) * 100; 
+  }
+}
